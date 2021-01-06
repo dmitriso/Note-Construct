3c1657a4eb70664fd142dbd4e3cbb7582218ea59
Dmitri So <dmitriso@yahoo.com>
2021-01-05 20:47:34 -0500

    Creating server instance and requiring modules.

--- server.js
+++ server.js
@@ -1,44 +1,26 @@
-// const express = require("express");
-// const path = require("path");
-// const fs = require("fs");
-// const notes = require('./db/db.json');
-
-// var app = express();
-// var PORT = process.env.PORT || 3000;
-
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
-// app.use(express.static('public'));
-
-// // HTML Routes
-// app.get('/notes', function(req,res) {
-//     res.sendFile(path.join(__dirname, 'public/notes.html'));
-// });
-
-// app.get('*', function(req,res) {
-//     res.sendFile(path.join(__dirname, 'public/index.html'));
-// });
+//VARIABLES AND REQUIRED MODULES
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+const uuidv1 = require('uuidv1')
+const notes = JSON.parse(fs.readFileSync('./db/db.json'));
 
 
-// // API Routes
-// app.get('/api/notes', function(req,res) {
-//     const notes = JSON.parse(fs.readFileSync('./db/db.json'));
-//     console.log(notes);
-//     return res.json(notes);
-// });
+var app = express();
+var PORT = process.env.PORT || 3000;
 
-// app.post('/api/notes', function(req,res) {
-//     const notes = JSON.parse(fs.writeFileSync('./db/db.json'));
-//     const newNote = req.body;
-//     notes.push(newNote);
-//     return res.json(newNote);
-// });
+// MIDDLEWARE   
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
 
-// app.delete('/api/notes/:id', function(req,res) {
-    
-// });
 
-// // Server listen
-// app.listen(PORT, function() {
-//     console.log("App listening on PORT " + PORT);
-//   });
+
+
+
+
+
+//SERVER LISTEN
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
