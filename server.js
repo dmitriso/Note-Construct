466762e2092acbc6b0257999dd7394c3b0f760ff
Dmitri So <dmitriso@yahoo.com>
2021-01-06 15:45:33 -0500

    Troubleshooting

--- server.js
+++ server.js
@@ -2,10 +2,11 @@
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
-const uuidv1 = require('uuidv1')
+const uuidv1 = require('uuidv1');
 const util = require('util');
 const notes = JSON.parse(fs.readFileSync('./db/db.json'));
 const writeFileAsync = util.promisify(fs.writeFile);
+const readFileAsync = util.promisify(fs.readFile);
 
 
 var app = express();
@@ -25,16 +26,12 @@ app.get('/notes', function(req,res) {
 app.get('/', function(req,res) {
     res.sendFile(path.join(__dirname, './public/index.html'));
 });
-// THIS CATCHES ALL 404 ERRORS
-// app.get("*", function (req, res) {
-//     res.sendFile(path.join(__dirname, "/public/index.html"));
-// });
+
 
 
 // API ROUTES
 // THIS READS AND RETRIEVES ALL STORED NOTES IN THE DATABASE
 app.get('/api/notes', (req,res) => {
-    console.log(notes);
     res.json(notes);
     res.end();
 });
@@ -51,16 +48,22 @@ app.post('/api/notes', (req,res) => {
     res.end();
 });
 
-// THIS SEARCHES FOR A STORED NOTE BY ID AND DELETES IT
-app.delete('/api/notes/:id', (req,res) => {
-    const id = req.params.id;
-    const note = notes.filter(function(noteObject) {
-        return noteObject.id !== id;
-    });
-    fs.writeFileSync('./db/db.json', JSON.stringify(note));
-    res.json(note);
-    res.end(); 
+app.post("/api/notes", function (req, res) {
+    notesdb.push(req.body)
+    res.json(true);
 });
+
+// THIS SEARCHES FOR A STORED NOTE BY ID AND DELETES IT
+app.delete("/api/notes/:id", function (req, res) {
+    const noteData = JSON.parse(fs.readFileSync("db/db.json"));
+    const newNotes = noteData.filter(function (noteObj) {
+        return noteObj.id !== req.params.id;
+    });
+    fs.writeFileSync("db/db.json", JSON.stringify(newNotes));
+    res.json(newNotes);
+});
+
+
 
 //SERVER LISTEN
 app.listen(PORT, function() {
