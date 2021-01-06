b3f7f4ed85b0d3a82cc2b4cf341e9f0800eef34f
Dmitri So <dmitriso@yahoo.com>
2021-01-05 21:16:42 -0500

    Created API route to DELETE a selected note by id.

--- server.js
+++ server.js
@@ -15,10 +15,11 @@ app.use(express.json());
 app.use(express.static('public'));
 
 // HTML ROUTES
+// THIS DISPLAYS THE NOTE TAKING INTERFACE TO THE SERVER
 app.get('/notes', function(req,res) {
     res.sendFile(path.join(__dirname, './public/notes.html'));
 });
-
+// THIS DISPLAYS THE MAIN PAGE TO THE SERVER
 app.get('/', function(req,res) {
     res.sendFile(path.join(__dirname, './public/index.html'));
 });
@@ -31,6 +32,7 @@ app.get('/api/notes', (req,res) => {
     res.json(notes);
     res.end();
 });
+
 // THIS CREATES A NEW NOTE FROM THE REQUEST AND WRITES IT TO A JSON FILE
 // EACH NOTE IS GIVEN A SPECIFIC ID WHEN IT IS CREATED
 app.post('/api/notes', (req,res) => {
@@ -41,6 +43,17 @@ app.post('/api/notes', (req,res) => {
     console.log(notes);
     res.json(notes);
     res.end();
+});
+
+// THIS SEARCHES FOR A STORED NOTE BY ID AND DELETES IT
+app.delete('/api/notes/:id', (req,res) => {
+    const id = req.params.id;
+    const note = notes.filter(function(noteObject) {
+        return noteObject.id !== id;
+    });
+    fs.writeFileSync('./db/db.json', JSON.stringify(note));
+    res.json(note);
+    res.end(); 
 });
 
 //SERVER LISTEN
